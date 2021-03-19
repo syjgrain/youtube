d50b6be1f6736d3d7cc8f8c22bb233da3d52869e
syjgrain <52773121+syjgrain@users.noreply.github.com>
2021-03-19 10:51:45 +0900

    update

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,22 @@
-
+import React, { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCflQ8mekS_J1OOWrz3HjD6DQXF1hbJ-jc", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  },[]);
   return (
-    <h1>Hello</h1>
+    <VideoList videos = {videos} />
   );
 }
 
